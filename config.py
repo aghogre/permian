9da89087fc2bc64b31078ced62368faac74435ac
Anshul Ghogre <30301360+aghogre@users.noreply.github.com>
2018-08-21 16:40:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ Created on Tue Aug 21 09:27:17 2018
 import os
 
 argument_config = {
-    'azure_account_name': os.getenv('AZURE_ACCOUNT_NAME', 'randomtrees'),
-    'azure_account_key': os.getenv('AZURE_ACCOUNT_KEY', 'wvNLlB2cSHhB0OFPRhIQDv+1QBJ1CnwFt+AGfQnL8rTyKTCG90t1Z+aCepe25aol6CKneJYgvHJl5gMtHON7TQ=='),
+    'azure_account_name': os.getenv('AZURE_ACCOUNT_NAME', ''),
+    'azure_account_key': os.getenv('AZURE_ACCOUNT_KEY', ''),
     'azure_container': os.getenv('CONTAINER', 'tsnv'),
     'neo4j_host': os.getenv('NEO4J_HOST', 'http://Graph:test@localhost:7474'),
     
